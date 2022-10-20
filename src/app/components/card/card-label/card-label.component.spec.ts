4966be21487354866d6b3cb05939608ace3f6d3c
Vaniza Marchetto <15125967+Vaniza@users.noreply.github.com>
2022-10-20 12:05:29 -0300

    Create card-label.component.spec.ts

---
+++ src/app/components/card/card-label/card-label.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CardLabelComponent } from './card-label.component';
+
+describe('CardLabelComponent', () => {
+  let component: CardLabelComponent;
+  let fixture: ComponentFixture<CardLabelComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CardLabelComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(CardLabelComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
